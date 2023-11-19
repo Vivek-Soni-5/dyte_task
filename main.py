62d1f34e57a8c2bc74344de6d4c70c236fff6afe
vivek-soni-5 <sonivivek050@gmail.com>
2023-11-19 11:37:30 +0530

    final commit

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_error_logs():
         return render_template('log_query.html', result = "ok", data = error_logs)
     return render_template('log_query.html')
 
-@app.route('/insert_logs', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def insert_logs():
     if request.method == 'POST':
         data = request.get_json()
@@ -91,6 +91,85 @@ def insert_logs():
         return render_template('insert_log.html', result = "ok")
     
     return render_template('insert_log.html')
+
+@app.route('/multiple_log_query', methods = ['POST', 'GET'])
+def multiple_log_query():
+    if request.method == 'POST':
+        # Initialize an empty list to store tuples
+        additional_values = []
+        # querys = db.child("logs")
+
+        # Check each parameter in the request.form and add corresponding tuples
+        if 'level' in request.form:
+            option1 = 'level'
+            value1 = request.form['level']
+            if value1 != '':
+                additional_values.append((option1, value1))
+                # querys = querys.order_by_child('level').start_at(value1).end_at(value1)
+
+        if 'message' in request.form:
+            option2 = 'message'
+            value2 = request.form['message']
+            if value2 != '':
+                additional_values.append((option2, value2))
+                # querys = querys.order_by_child('message').start_at(value2).end_at(value2)
+
+        if 'resourceId' in request.form:
+            option3 = 'resourceId'
+            value3 = request.form['resourceId']
+            if value3 != '':
+                additional_values.append((option3, value3))
+                # querys = querys.order_by_child('resourceId').start_at(value3).end_at(value3)
+
+        if 'timestamp' in request.form:
+            option4 = 'timestamp'
+            value4 = request.form['timestamp']
+            if value4 != '':
+                additional_values.append((option4, value4))
+                # querys = querys.order_by_child('timestamp').start_at(value4).end_at(value4)
+
+        if 'traceId' in request.form:
+            option5 = 'traceId'
+            value5 = request.form['traceId']
+            if value5 != '':
+                additional_values.append((option5, value5))
+                # querys = querys.order_by_child('traceId').start_at(value5).end_at(value5)
+
+        if 'spanId' in request.form:
+            option6 = 'spanId'
+            value6 = request.form['spanId']
+            if value6 != '':
+                additional_values.append((option6, value6))
+                # querys = querys.order_by_child('spanId').start_at(value6).end_at(value6)
+
+        if 'commit' in request.form:
+            option7 = 'commit'
+            value7 = request.form['commit']
+            if value7 != '':
+                additional_values.append((option7, value7))
+                # querys = querys.order_by_child('commit').start_at(value7).end_at(value7)
+
+        if 'parentResourceId' in request.form:
+            option8 = 'metadata/parentResourceId'
+            value8 = request.form['parentResourceId']
+            if value8 != '':
+                additional_values.append((option8, value8))
+                # querys = querys.order_by_child('metadata/parentResourceId').start_at(value8).end_at(value8)
+
+        #Now you can use the additional_values list in your query
+        query = db.child("logs")
+        
+        print(additional_values)
+
+        for child_key, value in additional_values:
+            query = query.order_by_child(child_key).start_at(value).end_at(value)
+
+        data = query.get()
+        error_logs = [log.val() for log in data.each()]
+                            
+        return render_template('multiple_log_query.html', result = "ok", data = error_logs)
+            
+    return render_template('multiple_log_query.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
